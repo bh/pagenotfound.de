7c45d735676f1fa187e3d319ea07ee6861556bec
Benjamin Hedrich <code@pagenotfound.de>
2022-10-02 16:42:14 +0200

    Exclude imprint and privacy policy in robots.txt and sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,21 @@
 const siteMetadata = require('./data/siteMetadata.js')
 
+const globalExcludedPaths = ['/datenschutz', '/impressum']
+
 module.exports = {
   siteUrl: siteMetadata.siteUrl,
-  generateRobotsTxt: true, // (optional)
+  generateRobotsTxt: true,
+  exclude: globalExcludedPaths,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        disallow: globalExcludedPaths,
+      },
+    ],
+  },
 }
