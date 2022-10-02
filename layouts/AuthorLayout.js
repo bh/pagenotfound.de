8b301685a7cf800affb8e7fc7da9831ced596a82
Benjamin Hedrich <code@pagenotfound.de>
2022-10-02 16:31:37 +0200

    CSS for h1, h2 and h3

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -10,9 +10,7 @@ export default function AuthorLayout({ children, frontMatter }) {
       <PageSEO title={`Über - ${name}`} description={`Über mich - ${name}`} />
       <div className="divide-y">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Über
-          </h1>
+          <h1>Über</h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center pt-8">
